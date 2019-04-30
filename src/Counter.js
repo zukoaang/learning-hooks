1b3e3b241c29778b27670a4e5ddbbe99fbc9f2d1
ohadha <ohadha@checkpoint.com>
2019-04-30 11:07:00 +0300

    add loading

--- src/Counter.js
+++ src/Counter.js
@@ -3,17 +3,20 @@ import React, { useState, useEffect } from 'react';
 export default () => {
   const [ count, setCount ] = useState(0)
   const [ person, setPerson ] = useState(null)
+  const [ loading, setLoading ] = useState(true)
 
   const increment = () => setCount( count + 1 )
 
   useEffect(() => {
     document.title = `The count is ${ count }`
     async function fetchData() {
+      setLoading(true)
       const res = await fetch('https://api.randomuser.me/')
       const data = await res.json()
       const [ item ] =  data.results
       console.log(item)
       setPerson(item)
+      setLoading(false)
     }
     fetchData()
   }, [count])
@@ -22,7 +25,7 @@ export default () => {
     <div>
       <p>The count is { count }</p>
       <button onClick={ increment }>Increment</button>
-      { person && <div>{ person.name.title } { person.name.first } { person.name.last }</div> }
+      { loading ? <div> Component is loading ... </div> : <div>{ person.name.title } { person.name.first } { person.name.last }</div> }
     </div>
   )
 }
